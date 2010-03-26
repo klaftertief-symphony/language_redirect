3a2ea26a9c2698ff3a983721a0e5082eecd2995a
Jonas Coch <klaftertief@googlemail.com>
2010-03-26 17:13:16 +0100

    improved region support

--- events/event.language_redirect.php
+++ events/event.language_redirect.php
@@ -33,48 +33,56 @@
 		protected function __trigger(){
 			$current_language = $_REQUEST['language'];
 			$current_region = $_REQUEST['region'];
+			$current_language_code = $_REQUEST['region'] ? $_REQUEST['language'].'-'.$_REQUEST['region'] : $_REQUEST['language'];
 			
 			$result = new XMLElement('language-redirect');
 			
-			$supported_languages = explode(',', General::Sanitize($this->_Parent->Configuration->get('languages', 'language_redirect')));
-			$supported_languages = array_map('trim', $supported_languages);
-			$supported_languages = array_filter($supported_languages);
+			$supported_language_codes = explode(',', General::Sanitize($this->_Parent->Configuration->get('languages', 'language_redirect')));
+			$supported_language_codes = array_map('trim', $supported_language_codes);
+			$supported_language_codes = array_filter($supported_language_codes);
 			
-			if (is_array($supported_languages) and !empty($supported_languages)) {
-				if (isset($current_language)) { // no redirect, set current language in cookie
-					setcookie(__SYM_COOKIE_PREFIX__ . 'language', $current_language, time()+TWO_WEEKS, __SYM_COOKIE_PATH__);
+			// only do something when there is a set of supported languages defined
+			if (is_array($supported_language_codes) and !empty($supported_language_codes)) {
+				// no redirect, set current language and region in cookie
+				if (isset($current_language_code) and in_array($current_language_code, $supported_language_codes)) {
+					$Cookie = new Cookie(__SYM_COOKIE_PREFIX__ . 'language-redirect', TWO_WEEKS, __SYM_COOKIE_PATH__);
+					$Cookie->set('language', $current_language);
+					$Cookie->set('region', $current_region);
 				}
-				else { // redirect to language depending on cookie or browser settings
-					$current_path = $this->_env['param']['current-path'];
+				// redirect to language-code depending on cookie or browser settings
+				else {
+					$current_path = !isset($current_language_code) ? $this->_env['param']['current-path'] : substr($this->_env['param']['current-path'],strlen($current_language_code)+1);
 					$browser_languages = Lang::getBrowserLanguages();
 					foreach ($browser_languages as $language) {
-						$language = substr($language,0,2);
-						if (in_array($language, $supported_languages)) {
+						if (in_array($language, $supported_language_codes)) {
 							$in_browser_languages = true;
 							$browser_language = $language;
 							break;
 						};
 					}
-					if (isset($_COOKIE[__SYM_COOKIE_PREFIX__ . 'language'])) {
-						$language = $_COOKIE[__SYM_COOKIE_PREFIX__ . 'language'];
+					$Cookie = new Cookie(__SYM_COOKIE_PREFIX__ . 'language-redirect', TWO_WEEKS, __SYM_COOKIE_PATH__);
+					$cookie_language_code = $Cookie->get('language');
+					if (strlen($cookie_language_code) > 0) {
+						$language_code = $Cookie->get('region') ? $cookie_language_code.'-'.$Cookie->get('region') : $cookie_language_code;
 					}
 					elseif ($in_browser_languages) {
-						$language = $browser_language;
+						$language_code = $browser_language;
 					}
 					else {
-						$language = $supported_languages[0];
+						$language_code = $supported_language_codes[0];
 					}
-					header ('Location: '.$this->_env['param']['root'].'/'.$language.$current_path);
+					// redirect and exit
+					header('Location: '.$this->_env['param']['root'].'/'.$language_code.$current_path);
 					die();
 				}
 				
-				$current_language_code = new XMLElement('current-language-code', $current_language);
-				$current_language_code->setAttribute('language', $current_language);
-				if (strlen($current_region) > 0) $current_language_code->setAttribute('region', $current_region);
-				$result->appendChild($current_language_code);
+				$current_language_code_xml = new XMLElement('current-language-code', $current_language_code);
+				$current_language_code_xml->setAttribute('language', $current_language);
+				if (strlen($current_region) > 0) $current_language_code_xml->setAttribute('region', $current_region);
+				$result->appendChild($current_language_code_xml);
 				
-				$supported_languages_xml = new XMLElement('supported-languages');
-				foreach($supported_languages as $language) {
+				$supported_languages_xml = new XMLElement('supported-language-codes');
+				foreach($supported_language_codes as $language) {
 					$language_code = new XMLElement('item', $language);
 					$region = substr(strrchr($language, '-'),1);
 					$language_code->setAttribute('language', substr($language,0,2));
@@ -82,6 +90,7 @@
 					$supported_languages_xml->appendChild($language_code);
 				}
 				$result->appendChild($supported_languages_xml);
+				
 				return $result;
 			}
 			return false;
